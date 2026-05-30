9d3e7e3d32a197d9188efe673f2a80637e993bb4
evftrya <evfi29@gmail.com>
2026-05-31 03:51:40 +0700

    sv

--- resources/views/kelola_data/jenjang-pendidikan/list.blade.php
+++ resources/views/kelola_data/jenjang-pendidikan/list.blade.php
@@ -84,7 +84,7 @@
                             <div class="flex items-center justify-center gap-3">
                                 <a
                                 href="{{ route('manage.jenjang-pendidikan.history', ['idUser' => $result->id]) }}"
-                                data-bs-target="#formasi-update" data-bs-toggle="modal" onclick="open_modal(this)"
+                                data-bs-target="#formasi-update" data-bs-toggle="modal"
                                     class="px-3 py-1.5 border open-modal border-[#0070ff] text-[#0070ff] rounded-md text-xs font-medium hover:bg-[#0070ff] hover:text-white transition">
                                     View Data
                             </a>
