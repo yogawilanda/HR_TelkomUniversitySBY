2953d54cb9eb862c13ac0f4ff58770898b91790d
evftrya <evfi29@gmail.com>
2026-05-31 06:07:42 +0700

    sv

--- resources/views/kelola_data/jenjang-pendidikan/update.blade.php
+++ resources/views/kelola_data/jenjang-pendidikan/update.blade.php
@@ -26,7 +26,7 @@
         <div class="flex w-full flex-col gap-[2.9373700618743896px] grow">
             <div class="flex items-center gap-[5.874740123748779px] self-stretch">
                 <span class="font-medium text-2xl leading-[20.56159019470215px] text-[#101828]">
-                    Update Jenjang Pendidikan {{ isset($user_data) && $user_data->id == session('account')['id']? 'Anda' : 'Pegawai' }} {{ $user_data->nama_lengkap??session('account')['nama_lengkap'] }}
+                    Update Jenjang Pendidikan {{ isset($user_data) && $user_data->id == session('account')['id']? 'Anda' : 'Pegawai' }} {{ session('account')['nama_lengkap']??$user_data->nama_lengkap }}
                 </span>
             </div>
         </div>
