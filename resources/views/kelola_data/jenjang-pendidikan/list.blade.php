7cce01fd4eaf6671ee9d3c2b18d5ffd31f4113f4
evftrya <evfi29@gmail.com>
2026-05-31 03:45:53 +0700

    sv

--- resources/views/kelola_data/jenjang-pendidikan/list.blade.php
+++ resources/views/kelola_data/jenjang-pendidikan/list.blade.php
@@ -95,9 +95,8 @@
                                     <ul class="dropdown-menu">
                                         @if ($result->id_pendidikan_tertinggi != null)
                                             <li>
-                                                {{ dd($result->pendidikan_data->id) }}
                                                 <a
-                                                    href="{{ route('manage.jenjang-pendidikan.update', ['id_jp' => $result['pendidikan_data']['id']]) }}"
+                                                    href="{{ route('manage.jenjang-pendidikan.update', ['id_jp' => $result->pendidikan_data->id]) }}"
                                                     class="dropdown-item hover:bg-blue-500 hover:text-white" href="#">
                                                         Ubah Data
                                                 </a>
