30b93d90dd2824c0037e6b23576eb5bee1f498e6
evftrya <evfi29@gmail.com>
2026-05-31 03:41:01 +0700

    sv

--- resources/views/kelola_data/jenjang-pendidikan/list.blade.php
+++ resources/views/kelola_data/jenjang-pendidikan/list.blade.php
@@ -65,7 +65,6 @@
 
             <x-slot:table_column>
                 @forelse ($results as $result)
-                {{ dd($result['pendidikan_data']['id']) }}
                     <x-tb-cl id="">
                         <x-tb-cl-fill>
 
