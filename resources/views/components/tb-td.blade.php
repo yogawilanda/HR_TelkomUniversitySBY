768e7c61934b4b917063e9f9db563b7008e486db
evftrya <evfi29@gmail.com>
2026-07-15 17:02:09 +0700

    save

--- resources/views/components/tb-td.blade.php
+++ resources/views/components/tb-td.blade.php
@@ -8,7 +8,7 @@
 @endphp
 
 <th data-field="{{ $nama }}"
-    data-filter-control="{{ $type }}"
+    data-filter-control="{{ $type==null?'input':$type }}"
     @if ($url_value_for_this_select) data-filter-default="{{ $url_value_for_this_select }}" @endif
     @if ($sorting) data-sortable="true" @endif
     class="px-6 py-4 text-[13px] font-bold text-[#1d1d1f] uppercase tracking-tight @if ($sorting) sortable @endif">
